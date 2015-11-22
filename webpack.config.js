adf9899de7b72ac709434717369cfb0b7fe1fa7e
Ricardo Mendes <ricardoamendes@gmail.com>
2015-11-22 23:07:27 +0000

    Adds env config support for dev and production

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 var path = require('path');
+var envs = require('gulp-environments');
 
 module.exports = {
-  entry: './app/scripts/main.js',
-  output: {
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.json$/,
-      loader: "json-loader"
-    }, {
-      test: /\.js$/,
-      exclude: [
-        path.join(__dirname, "node_modules")
-      ],
-      loader: 'babel-loader'
-    }]
-  }
+    entry: './app/scripts/main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        alias: {
+            config: path.join(__dirname, 'app/config', envs.production() ? 'production' : 'development')
+        }
+    },
+    module: {
+        loaders: [{
+            test: /\.json$/,
+            loader: 'json-loader'
+        }, {
+            test: /\.js$/,
+            exclude: [
+                path.join(__dirname, 'node_modules')
+            ],
+            loader: 'babel-loader'
+        }]
+    }
 };
