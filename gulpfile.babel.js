5c06b1400a658def8bb373c20daf357f153af30a
Ricardo Mendes <ricardoamendes@gmail.com>
2015-11-22 23:09:35 +0000

    Adds support for different environments and tasks to build and deploy the web app in production

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,71 +1,133 @@
+// Gulp task file. Usage:
+//  gulp dev
+//  gulp prod
+//  gulp serve
+//  gulp release
+
 'use strict';
 
-// Load plugins
+// Core and dev helper modules
 import gulp from 'gulp';
 import webpack from 'gulp-webpack';
+import envs from 'gulp-environments';
+import browserSync from 'browser-sync';
+import scp from 'gulp-scp';
+import config from './config.json';
+
+// Styles helper modules
 import sass from 'gulp-ruby-sass';
 import autoprefixer from 'gulp-autoprefixer';
+import concatcss from 'gulp-concat-css';
+
+// Scripts helper modules
 import jshint from 'gulp-jshint';
-import browserSync from 'browser-sync';
+import inlinesource from 'gulp-inline-source';
+import minifyhtml from 'gulp-minify-html';
+
+const dev = envs.development;
+const prod = envs.production;
 
 // Source and build folders
 const dirs = {
-  src: 'app',
-  build: {
-    local: 'build/local'
-  }
+    app: 'app',
+    server: 'server',
+    dev: 'build/dev',
+    prod: 'build/prod'
 };
 
 // Source and build file paths
 const paths = {
-  src: {
-    html: `${dirs.src}/index.html`,
-    scripts: `${dirs.src}/scripts/**/*.js`,
-    styles: `${dirs.src}/styles/**/*.scss`
-  },
-  build: {
-    local: {
-      html: `${dirs.build.local}/index.html`,
-      scripts: `${dirs.build.local}/js/`,
-      styles: `${dirs.build.local}/css/`
+    src: {
+        html: `${dirs.app}/index.html`,
+        scripts: `${dirs.app}/scripts/**/*.js`,
+        styles: [`${dirs.app}/styles/**/*.scss`],
+        assets: [`${dirs.app}/assets`]
+    },
+    dev: {
+        html: `${dirs.dev}/index.html`,
+        scripts: `${dirs.dev}/js/`,
+        styles: `${dirs.dev}/css/`,
+        assets: `${dirs.dev}/assets/`
+    },
+    prod: {
+        app: `${dirs.prod}/index.html`,
+        server: `${dirs.server}/server.js`
     }
-  }
 };
 
 // Process index.html
 gulp.task('html', () => {
-  return gulp.src(paths.src.html)
-    .pipe(gulp.dest(dirs.build.local))
-    .pipe(browserSync.stream());
+    return gulp.src(paths.src.html)
+        .pipe(gulp.dest(dirs.dev))
+        .pipe(dev(browserSync.stream()));
+});
+
+// Scripts
+gulp.task('scripts', () => {
+    return gulp.src(paths.src.scripts)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest(paths.dev.scripts))
+        .pipe(dev(browserSync.stream()));
 });
 
 // Compile sass into CSS
 gulp.task('styles', () => {
-  return sass(paths.src.styles)
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest(paths.build.local.styles))
-    .pipe(browserSync.stream());
+    return sass(paths.src.styles)
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(concatcss('bundle.css'))
+        .pipe(gulp.dest(paths.dev.styles))
+        .pipe(dev(browserSync.stream()));
 });
 
-// Scripts
-gulp.task('scripts', function() {
-  return gulp.src(paths.src.scripts)
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest(paths.build.local.scripts))
-    .pipe(browserSync.stream());
+// Copy assets
+gulp.task('assets', () => {
+    return sass(paths.src.assets)
+        .pipe(gulp.dest(paths.dev.assets))
+        .pipe(dev(browserSync.stream()));
 });
+
+// Inline and minify sources
+gulp.task('optimize', () => {
+    return gulp.src(paths.dev.html)
+        .pipe(inlinesource())
+        .pipe(minifyhtml())
+        .pipe(gulp.dest(dirs.prod));
+});
+
+// Deploy production client and server scripts to server
+gulp.task('deploy', function() {
+    return gulp.src([paths.prod.app, paths.prod.server])
+        .pipe(scp(config.server));
+});
+
+// // Set development flag
+gulp.task('set-dev', dev.task);
+//
+// // Set production flag
+gulp.task('set-prod', prod.task);
+
+// Build pipeline task
+gulp.task('build', ['html', 'scripts', 'styles', 'assets']);
+
+// Dev build task
+gulp.task('dev', ['set-dev', 'build']);
+
+// Prod build task
+gulp.task('prod', ['set-prod', 'build', 'optimize']);
+
+// Build and release task
+gulp.task('release', ['prod', 'deploy']);
 
 // Static server + watching scss/html files
-gulp.task('serve', ['html', 'scripts', 'styles'], () => {
-  browserSync.create();
-  browserSync.init({
-    server: `${dirs.build.local}`
-  });
-  gulp.watch(paths.src.html, ['html']);
-  gulp.watch(paths.src.scripts, ['scripts']);
-  gulp.watch(paths.src.styles, ['styles']);
+gulp.task('serve', ['set-dev', 'build'], () => {
+    browserSync.create();
+    browserSync.init({
+        server: `${dirs.dev}`
+    });
+    gulp.watch(paths.src.html, ['html']);
+    gulp.watch(paths.src.scripts, ['scripts']);
+    gulp.watch(paths.src.styles, ['styles']);
+    gulp.watch(paths.src.assets, ['assets']);
 });
-
-gulp.task('default', ['serve']);
