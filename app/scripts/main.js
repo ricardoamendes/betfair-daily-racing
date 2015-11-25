739fe0f52e33956d16974b2bec1aacdabbd74f60
Ricardo Mendes <ricardoamendes@gmail.com>
2015-11-25 01:15:11 +0000

    Reformatting client side templates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,42 +11,48 @@ var events = {
 
 /**
  * Generate a DOM tree for event runners.
- * @param  {Array} runners Runner names and pricing data
- * @return {Array[HTML]}          The DOM tree
+ * @param  {Array} runners  Runner names and pricing data
+ * @return {Array[HTML]}    The DOM tree
  */
-var renderFavourites = (runners) => {
+var renderFavouritePrices = (runners) => {
 
     // Hold final markup
-    var renderedFavourites = [];
+    var prices = [];
 
     // Limit up to 3 favorites
     runners = runners.slice(0, 3);
 
     // Sort runners by lowest price
     runners.sort((runner1, runner2) => {
-        var price1 = runner1.ex && runner1.ex.availableToBack.length && runner1.ex.availableToBack[0].price,
-            price2 = runner2.ex && runner2.ex.availableToBack.length && runner2.ex.availableToBack[0].price;
+        let price1 = runner1.ex &&
+                     runner1.ex.availableToBack.length &&
+                     runner1.ex.availableToBack[0].price,
+            price2 = runner2.ex &&
+                     runner2.ex.availableToBack.length &&
+                     runner2.ex.availableToBack[0].price;
 
         return price1 > price2 ? 1 : price1 < price2 ? -1 : 0;
     });
 
     // Push runner markup
     runners.map((runner) => {
+        let price1 = runner.ex &&
+                     runner.ex.availableToBack.length &&
+                     runner.ex.availableToBack[0].price,
+            price2 = runner.ex &&
+                     runner.ex.availableToLay.length &&
+                     runner.ex.availableToLay[0].price;
+
         if (runner) {
-            renderedFavourites.push(m('div', {
-                class: 'events__table__row__cell events__table__row__cell__odds'
-            }, [
-                m('div', {
-                    class: 'events__table__row__cell__odds__back'
-                }, runner.ex && runner.ex.availableToBack.length && runner.ex.availableToBack[0].price),
-                m('div', {
-                    class: 'events__table__row__cell__odds__lay'
-                }, runner.ex && runner.ex.availableToLay.length && runner.ex.availableToLay[0].price)
-            ]));
+            prices.push(
+                m('td', { class: 'events__body__row__cell events__body__row__cell__odds' },
+                    m('div', { class: 'events__body__row__cell__odds__back' }, price1),
+                    m('div', { class: 'events__body__row__cell__odds__lay' }, price2)
+            ));
         }
     });
 
-    return renderedFavourites;
+    return prices;
 };
 
 /**
@@ -70,41 +76,35 @@ events.model = [];
 
 // Events view
 events.view = function() {
-    return m('div', {
-        class: 'events'
-    }, [m('div', {
-            class: 'events__status'
-        }), m('div', {
-            class: 'events__header'
-        }, [
-            m('div', 'Start Time'),
-            m('div', 'Venue'),
-            m('div', 'Market'),
-            m('div', '1st Favourite'),
-            m('div', '2nd Favourite'),
-            m('div', '3rd Favourite')
-        ]),
-        m('div', {
-            class: 'events__table'
-        }, [
-            events.model.map(function(data, index) {
-                return m('div', {
-                    class: 'events__table__row'
-                }, [
-                        m('div', {
-                        class: 'events__table__row__cell'
-                    }, data.marketStartTime.match(/\d{2}:\d{2}/)),
-                        m('div', {
-                        class: 'events__table__row__cell'
-                    }, data.event.venue),
-                        m('div', {
-                        class: 'events__table__row__cell'
-                    }, data.marketName),
-                        renderFavourites(data.runners)
-                    ]);
-            })
-        ])
-    ]);
+    let columns = ['Start Time', 'Venue', 'Market', '1st Favourite', '2nd Favourite', '3rd Favourite'];
+
+    return (
+        m('table', { class: 'events' },
+
+            // Heading columns
+            m('thead',
+                m('tr', { class: 'events__header__row' },
+                    columns.map(column => m('th', { class: 'events__header__row__cell' }, column)))),
+
+            // Content body
+            m('tbody', { class: 'events__body' },
+
+                // Body rows
+                events.model.map((data, index) => {
+                    return (
+                        m('tr', { class: 'events__body__row' },
+                            m('td', { class: `events__body__row__cell events__body__row__cell__flag--${data.event.countryCode}` }, data.marketStartTime.match(/\d{2}:\d{2}/)),
+                            m('td', { class: 'events__body__row__cell' }, data.event.venue),
+                            m('td', { class: 'events__body__row__cell' }, data.marketName),
+
+                            // Favourite back/lay odds
+                            renderFavouritePrices(data.runners)
+                        )
+                    );
+                })
+            )
+        )
+    );
 };
 
 // Mount the events view into DOM
