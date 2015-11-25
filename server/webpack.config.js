fa3b5ee5ad5699e27488a150146eb7aa1fbb245f
Ricardo Mendes <ricardoamendes@gmail.com>
2015-11-25 01:17:13 +0000

    Adding an alias to the config file

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,21 +1,26 @@
 var path = require('path');
 
 module.exports = {
-  entry: './main.js',
-  target: 'node',
-  output: {
-    filename: 'server.js'
-  },
-  module: {
-    loaders: [{
-      test: /\.json$/,
-      loader: "json-loader"
-    }, {
-      test: /\.js$/,
-      exclude: [
-         path.join(__dirname, "node_modules")
-      ],
-      loader: 'babel-loader'
-    }]
-  }
+    entry: './main.js',
+    target: 'node',
+    output: {
+        filename: 'server.js'
+    },
+    resolve: {
+        alias: {
+            config: path.join(__dirname, 'scripts', 'config')
+        }
+    },
+    module: {
+        loaders: [{
+            test: /\.json$/,
+            loader: "json-loader"
+        }, {
+            test: /\.js$/,
+            exclude: [
+                path.join(__dirname, "node_modules")
+            ],
+            loader: 'babel-loader'
+        }]
+    }
 };
